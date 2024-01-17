121384803925def7bb2f9c6c4613dd1d30d09222
dreamchach <114228865+dreamchach@users.noreply.github.com>
2024-01-17 19:35:40 +0900

    Update defaultAxios.js

--- src/utill/axios/defaultAxios.js
+++ src/utill/axios/defaultAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : 'http://https://port-0-sns-app-back-koh2xlj3ufoqd.sel4.cloudtype.app'
+    baseURL : 'https://port-0-sns-app-back-koh2xlj3ufoqd.sel4.cloudtype.app'
 })
 
 export default instance
