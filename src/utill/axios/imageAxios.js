cd2db62fb2d994875471a578133e82121f5cd441
dreamchach <114228865+dreamchach@users.noreply.github.com>
2024-01-17 19:35:51 +0900

    Update imageAxios.js

--- src/utill/axios/imageAxios.js
+++ src/utill/axios/imageAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const imageInstance = axios.create({
-    baseURL : 'http://https://port-0-sns-app-back-koh2xlj3ufoqd.sel4.cloudtype.app',
+    baseURL : 'https://port-0-sns-app-back-koh2xlj3ufoqd.sel4.cloudtype.app',
     headers : {'Content-Type' : 'multipart/form-data'}
 })
 
